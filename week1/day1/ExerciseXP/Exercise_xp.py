2cd2c7e589e8aac564487b06e6e67915feec18a0
TomBen12 <tom.benitah1@gmail.com>
2025-02-02 13:55:40 +0200

    day1 Exercise_xp commited!

--- week1/day1/ExerciseXP/Exercise_xp.py
+++ week1/day1/ExerciseXP/Exercise_xp.py
@@ -1,43 +1,61 @@
-print('Exercise 1: Hello World')
-print('Hello world\n' * 4 )
-print('----------------------')
+# print('Exercise 1: Hello World\n')
+# print('Hello world\n' * 4 )
+# print('----------------------')
 
-print('Exercise 2: Some Math')
-result = (99**3)*8
-print(result)
-print('----------------------')
+# print('Exercise 2: Some Math')
+# result = (99**3)*8
+# print(result)
+# print('----------------------')
 
-print('Exercise 3 : What is the output ?')
-print('a: False')
-print('b: True')
-print('c: False')
-print('d: type error') #type error!!
-print('e: False')
-print('----------------------')
+# print('Exercise 3 : What is the output ?\n')
+# print('a: False')
+# print('b: True')
+# print('c: False')
+# print('d: type error') #type error!!
+# print('e: False')
+# print('----------------------')
 
-print('Exercise 4 : Your computer brand')
-computer_brand = 'mac'
-print(f'I have a {computer_brand} computer')
-print('----------------------')
+# print('Exercise 4 : Your computer brand\n')
+# computer_brand = 'mac'
+# print(f'I have a {computer_brand} computer')
+# print('----------------------')
 
-print('Exercise 5 : Your information')
-name = 'Tom'
-age = 30
-shoe_size = 42
-info = f'My name is {name}, I am {age} years old,I love video games and my shoe size is {shoe_size}.'
-print(info)
-print('----------------------')
+# print('Exercise 5 : Your information\n')
+# name = 'Tom'
+# age = 30
+# shoe_size = 42
+# info = f'My name is {name}, I am {age} years old,I love video games and my shoe size is {shoe_size}.'
+# print(info)
+# print('----------------------')
 
-print('Exercise 6 : A & B')
-a = 5
-b = 10
-if a > b:
-    print('hello world')
-else: print('bye world')
-print('----------------------')
+# print('Exercise 6 : A & B\n')
+# a = 5
+# b = 10
+# if a > b:
+#     print('hello world')
+# else: print('bye world')
+# print('----------------------')
 
-print('Exercise 7 : Odd or Even')
-user_number = input('write a number')
+# print('Exercise 7 : Odd or Even\n')
+# user_number = int(input('write a number (but not zero ;)) : '))
+# if user_number == 0:
+#     print('zeros are not odd or even numbers')
+# elif user_number %2 == 0:
+#     print('you picked an even number')
+# else: print('you picked an odd number')
+# print('----------------------')
 
+# print("Exercise 8 : What's your name?\n")
+# my_name = 'tom'
+# user_name = input('Hey! What is your name? ').lower()
+# if user_name == my_name:
+#     print('That is the most awesome name EVERRR ;) ')
+# else: print('what a great name!')
+# print('----------------------')
 
-
+# print('Exercise 9 : Tall enough to ride a roller coaster instructions\n')
+# minimum_height = 145
+# user_height = int(input('Write your height in CENTIMETERS! ... please: '))
+# if user_height > minimum_height :
+#     print('get in big boy!')
+# else: print('bye bye shorty, come back in a few years.')
