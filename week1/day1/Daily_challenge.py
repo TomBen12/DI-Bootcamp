93dc19184e17827cd5f042962fa54e52401ff607
TomBen12 <tom.benitah1@gmail.com>
2025-02-02 16:14:49 +0200

    Day 1 Xp gold commited

--- week1/day1/Daily_challenge.py
+++ week1/day1/Daily_challenge.py
@@ -1,26 +1,26 @@
-import random
+# import random
 
-print('Daily Challenge')
-print('1-------------------------')
+# print('Daily Challenge')
+# print('1-------------------------')
 
-user_string = (input('write a 10 character string: '))
-if len(user_string) < 10:
-    print('string not long enough')
-elif len(user_string) > 10:
-    print('string too long')
-else: print('perfect string ^_^')
+# user_string = (input('write a 10 character string: '))
+# if len(user_string) < 10:
+#     print('string not long enough')
+# elif len(user_string) > 10:
+#     print('string too long')
+# else: print('perfect string ^_^')
 
-print('2-------------------------')
+# print('2-------------------------')
 
-print(f'the first and last characters are: {user_string[0]+user_string[-1]}')
+# print(f'the first and last characters are: {user_string[0]+user_string[-1]}')
 
-print('3------------------------')
-y = ''
-for x in user_string:
-    y+=x
-    print(y)
+# print('3------------------------')
+# y = ''
+# for x in user_string:
+#     y+=x
+#     print(y)
 
-print('4------BONUS------------')
-string_letters = list(user_string)
-random.shuffle(string_letters)
-print("Everyday I'm shuffling:", "".join(string_letters))
+# print('4------BONUS------------')
+# string_letters = list(user_string)
+# random.shuffle(string_letters)
+# print("Everyday I'm shuffling:", "".join(string_letters))
--- week1/day1/Exercise_xp_gold.py
+++ week1/day1/Exercise_xp_gold.py
@@ -1,6 +1,19 @@
-print('Exercise 1 : Hello World -I love Python\n')
+# print('Exercise 1 : Hello World -I love Python\n')
 
-print('Hello world\n' * 4 + 'I love python\n' *4)
-print('-------------------------------------')
+# print('Hello world\n' * 4 + 'I love python\n' *4)
+# print('-------------------------------------')
 
-print()
+month = int(input("hey!... enter a month by number 1-12 and I'll tell you which season! :  "))
+
+if month > 12 :
+    print('WHAT HAVE YOU DONE!?')
+elif month < 0:
+    print("REALLY??!")
+elif month >=3 and month <=5 :
+    print("Spring Time!")
+elif month >=6 and month <=8:
+    print("Summer is HOTT!")
+elif month >=9 and month <=11 :
+    print("Autumn... Beautiful..")
+elif month == 12 or month <=2 :
+    print("WINTER IS COMMING")
