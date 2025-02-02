741229ce3d16d9e1376f7f87ea1079da3cdfad49
TomBen12 <tom.benitah1@gmail.com>
2025-02-02 16:18:30 +0200

    small change in day1 xp gold

--- week1/day1/Exercise_xp_gold.py
+++ week1/day1/Exercise_xp_gold.py
@@ -1,7 +1,10 @@
-# print('Exercise 1 : Hello World -I love Python\n')
+print('Exercise 1 : Hello World -I love Python\n')
 
-# print('Hello world\n' * 4 + 'I love python\n' *4)
-# print('-------------------------------------')
+print('Hello world\n' * 4 + 'I love python\n' *4)
+print('-------------------------------------')
+
+
+print('Exercise 2 : What is the Season ?')
 
 month = int(input("hey!... enter a month by number 1-12 and I'll tell you which season! :  "))
 
