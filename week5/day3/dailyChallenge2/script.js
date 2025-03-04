d1fbb29fa14a7366a4f4b80d13ad26e076ddf0c4
TomBen12 <tom.benitah1@gmail.com>
2025-03-04 19:35:07 +0200

    week5 day3 dailychallenge commited !

---
+++ week5/day3/dailyChallenge2/script.js
@@ -0,0 +1,44 @@
+// const inventory = [
+//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+//   { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+//   {
+//     id: 4,
+//     car_make: "Land Rover",
+//     car_model: "Defender Ice Edition",
+//     car_year: 2010,
+//   },
+//   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+// ];
+
+// function getCarHonda(carInventory) {
+//   let wantedCar = carInventory.find((car) => car.car_make == "Honda");
+//   console.log(
+//     `this is a ${wantedCar.car_make} ${wantedCar.car_model} from ${wantedCar.car_year}`
+//   );
+
+//   return wantedCar;
+// }
+
+// getCarHonda(inventory);
+
+//part 2
+
+// const inventory = [
+//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+//   { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+//   {
+//     id: 4,
+//     car_make: "Land Rover",
+//     car_model: "Defender Ice Edition",
+//     car_year: 2010,
+//   },
+//   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+// ];
+
+// function sortCarInventoryByYear(carInventory) {
+//   carInventory.sort((car, car2) => car.car_year - car2.car_year);
+// }
+
+// sortCarInventoryByYear(inventory);
