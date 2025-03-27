42ff37f75696a6b90a17d22944ae08e546dcf022
TomBen12 <tom.benitah1@gmail.com>
2025-03-27 08:32:39 +0200

    w8d1 react-intro daily challenge commit

---
+++ week8/day1/exercises/dailychallenge/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ImgCarousel from "./components/ImgCarousel.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Carousel</h1>
+      <ImgCarousel />
+    </div>
+  );
+}
+
+export default App;
---
+++ week8/day1/exercises/dailychallenge/src/components/ImgCarousel.jsx
@@ -0,0 +1,27 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const ImgCarousel = () => {
+  return (
+    <Carousel>
+      <div>
+        <img src="/hongkong.jpg" alt="Hong Kong" />
+        <p className="legend">Hong Kong</p>
+      </div>
+      <div>
+        <img src="/japan.webp" alt="Japan" />
+        <p className="legend">Japan</p>
+      </div>
+      <div>
+        <img src="/macao.webp" alt="Macao" />
+        <p className="legend">Macao</p>
+      </div>
+      <div>
+        <img src="/vegas.webp" alt="Las Vegas" />
+        <p className="legend">Las Vegas</p>
+      </div>
+    </Carousel>
+  );
+};
+
+export default ImageCarousel;
